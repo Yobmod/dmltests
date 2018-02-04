5456af749b5b7b2daa8ff7cda5553d0388a1ead8
yobmod <yobmod@gmail.com>
2018-02-04 21:08:13 +0000

    add ttk style

--- python/bf/bf_tk.py
+++ python/bf/bf_tk.py
@@ -1,45 +1,51 @@
 import tkinter as tk
-# from tkinter import ttk
+from tkinter import ttk
 from typing import Optional  # , Union, Any
 import brainfuck
 
 
 class Application(tk.Frame):
-	def __init__(self, master: Optional[tk.Tk]=None) -> None:
-		super().__init__(master)
-		self.pack()
-		self.create_widgets()
 
-	def create_widgets(self) -> None:
-		self.hi_there = tk.Button(self)
-		self.hi_there["text"] = "Hello World\n(click me)"
-		self.hi_there["command"] = self.say_hi
-		self.hi_there.pack(side="top")
+    def __init__(self, master: Optional[tk.Tk]=None) -> None:
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+        style = ttk.Style()
+        style.configure("BW.TButton", foreground="red", background="red")
 
-		self.bf_button = tk.Button(self, text="Brainfuck", command=self.bf_command)
-		self.bf_button.pack(side="left")
 
-		self.quit_button = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
-		self.quit_button.pack(side="bottom")
+    def create_widgets(self) -> None:
 
-	"""
-	* radiobutton for src = input field OR src = file
-	* bf button command takes source and outputs
-	* optional input fields for bf() in subwindow
-	"""
+        self.hi_there = ttk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        #self.hi_there["style"] = "BW.TButton"
+        self.hi_there.pack(side="top")
 
-	def bf_command(self) -> None:
-		bf_src = self.bf_src()
-		bf_outputstr = brainfuck.bf(bf_src)
-		# len(bf_outputstr))
-		print(bf_outputstr)
+        self.bf_button = ttk.Button(self, text="Brainfuck", command=self.bf_command)
+        self.bf_button.pack(side="left")
 
-	def bf_src(self) -> str:
-		src = "+.>+++.>+++++++++++.>"
-		return src
+        self.quit_button = ttk.Button(self, text="QUIT", style="BW.TButton", command=root.destroy)
+        self.quit_button.pack(side="bottom")
 
-	def say_hi(self) -> None:
-		print("hi there, everyone!")
+    """
+    * radiobutton for src = input field OR src = file
+    * bf button command takes source and outputs
+    * optional input fields for bf() in subwindow
+    """
+
+    def bf_command(self) -> None:
+        bf_src = self.bf_src()
+        bf_outputstr = brainfuck.bf(bf_src)
+        # len(bf_outputstr))
+        print(bf_outputstr)
+
+    def bf_src(self) -> str:
+        src = "+.>+++.>+++++++++++.>"
+        return src
+
+    def say_hi(self) -> None:
+        print("hi there, everyone!")
 
 
 root = tk.Tk()
