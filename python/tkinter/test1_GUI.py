61c1d0cb066905ba3c6ccc5485a207fd79f5baa4
Laventine <yobmod@gmail.com>
2018-02-15 12:58:00 +0000

    tk tests

--- python/tkinter/test1_GUI.py
+++ python/tkinter/test1_GUI.py
@@ -15,22 +15,27 @@ class Example(ttk.Frame):
 	def initui(self) -> None:
 		self.parent.title("Absolute positioning")
 		self.pack(fill=tk.BOTH, expand=1)
-		ttk.Style().configure("TFrame", background="#333")
-		# ttk.Style().configure("TLabel", borderwidth=10)
+		style = ttk.Style()
+		style.configure("TFrame", background="#333")
+		style.configure("dml.TLabel", background="white")
 
-		bard = Image.open("lighthouse.jpg")
+		bard = Image.open("lighthouse.jpg").resize((300, 200))
 		bardejov = ImageTk.PhotoImage(bard)
-		label1 = tk.Label(self, image=bardejov, borderwidth=5)
+		label1 = tk.Label(self, image=bardejov, borderwidth=10)
 		label1.image = bardejov
 		label1.place(x=20, y=20)
+		# print(width)
 
 		rot = Image.open("Penguins.jpg")
 		rotunda = ImageTk.PhotoImage(rot)
-		label2 = ttk.Label(self, image=rotunda)  # , style="TLabel")
+		label2 = ttk.Label(self, image=rotunda, style="dml.TLabel")
 		label2.image = rotunda
 		label2.place(x=40, y=160)
 
 		minc = Image.open("Tulips.jpg")
+		width, height = minc.size
+		ratio = 2; new_size = (int(width / ratio), int(height / ratio))
+		minc = minc.resize(new_size)
 		mincol = ImageTk.PhotoImage(minc)
 		label3 = tk.Label(self, image=mincol)
 		label3.image = mincol
@@ -41,8 +46,8 @@ def main(width: int=600, height: int=500) -> None:
 	size = str(width) + "x" + str(height)
 	root = tk.Tk()
 	root.geometry(size)  # widthxheight
-	# app = Example(root)
-	root.mainloop()
+	app = Example(root)
+	app.mainloop()
 
 
 if __name__ == '__main__':
