5d40c0dfad916abe972bd01668190caaae8a9da7
yobmod <yobmod@gmail.com>
2018-09-15 00:40:01 +0100

    FMT 7

---
+++ jinja/fljn/app/errors.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from app import app, db
+
+from app.routes import HTML
+from typing import Tuple
+
+@app.errorhandler(404)
+def not_found_error(error: Exception) -> Tuple[HTML, int]:
+    rendered: HTML = render_template('404.html')
+    return (rendered, 404)
+
+
+@app.errorhandler(500)
+def internal_error(error: Exception) -> Tuple[HTML, int]:
+    db.session.rollback()
+    rendered: HTML = render_template('500.html')
+    return rendered, 500
