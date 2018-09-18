337f4d05ce013ef551cd1c074e8b1dc6bb7818f9
Laventine <yobmod@gmail.com>
2018-09-18 12:14:46 +0100

    FMT 12

--- flask/src/app/__init__.py
+++ flask/src/app/__init__.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -21,6 +22,7 @@ login = LoginManager(app)
 login.login_view = 'login'  # name of view
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors, email
 
