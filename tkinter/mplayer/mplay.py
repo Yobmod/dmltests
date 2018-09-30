eb5d30c8a2043435fae01805075e2e362136a679
yobmod <yobmod@gmail.com>
2018-09-30 22:03:07 +0100

    finish Mplayer challenge

--- tkinter/mplayer/mplay.py
+++ tkinter/mplayer/mplay.py
@@ -1,20 +1,39 @@
-import tkinter as tk
-from tkinter import (Menu, Label, PhotoImage, Button, Scale, Listbox,
-                     messagebox, filedialog,
-                     HORIZONTAL, TRUE, FALSE, LEFT, BOTTOM, X, W,
-                     SUNKEN, GROOVE, )
+from tkinter import (messagebox, filedialog, PhotoImage, Menu, Listbox,  # Frame, Label, Scale, Button,
+                     HORIZONTAL, LEFT, BOTTOM, X, W, SUNKEN, GROOVE, )
+from tkinter.ttk import Button, Scale, Label, Frame
+# import tkinter as tk
+# from tkinter import ttk
+from ttkthemes import themed_tk as tk
 from pygame import mixer
 from mutagen.mp3 import MP3  # ASF, FLAC, MP4, OGG
 import os
 import threading
 import time
+import logging
+from dotenv import load_dotenv
 
 from typing import List, Tuple
 
-"""Are TRUE / FALSE needed?"""
+
+# set DEBUG and logger
+mplayer_path = os.path.dirname(os.path.abspath(__file__))
+parent_path = os.path.dirname(mplayer_path)
+devenv_path = os.path.join(parent_path, '.env')
+load_dotenv(devenv_path)
+DEBUG = os.environ.get("DEBUG") or False
+logger = logging.getLogger()
+if DEBUG: print("DEBUG mode on")
 
 
-root = tk.Tk()
+try:
+    root = tk.ThemedTk()
+    root.get_themes()                 # Returns a list of all themes that can be set
+    root.set_theme("plastik")
+except AttributeError as e:
+    root = tk.Tk()
+    if DEBUG: print("Themed Tk not loaded")
+
+
 mixer.init()  # initializing the mixer
 
 statusbar = Label(root, text="Welcome to Melody", relief=SUNKEN, anchor=W)
@@ -27,7 +46,9 @@ root.config(menu=menubar)
 # Set initial values
 init_vol = 70
 paused = False
-muted = FALSE
+muted = False
+playing = False
+current_song = None
 filename: str
 filename_path: str
 playlist: List[str] = []
@@ -39,6 +60,7 @@ def browse_file() -> None:
     global filename_path
     filename_path = filedialog.askopenfilename()
     add_to_playlist(filename_path)
+    mixer.music.queue(filename_path)
 
 
 def add_to_playlist(filename: str) -> None:
@@ -68,13 +90,13 @@ menubar.add_cascade(label="Help", menu=subMenu2)
 subMenu2.add_command(label="About Us", command=about_us)
 
 
-root.geometry('500x300')
-root.minsize(300, 300)
+root.geometry('600x400')
+root.minsize(400, 400)
 root.title("Melody")
-root.iconbitmap(R'assets\icons\melody.ico')
+root.iconbitmap(mplayer_path + R'\assets\icons\melody.ico')
 
 
-leftframe = tk.Frame(root)
+leftframe = Frame(root)
 leftframe.pack(side=LEFT, padx=30)
 
 playlistbox = Listbox(leftframe)
@@ -96,7 +118,7 @@ delBtn = Button(leftframe, text="- Del", command=del_song)
 delBtn.pack(side=LEFT)
 
 
-rightframe = tk.Frame(root)
+rightframe = Frame(root)
 rightframe.pack()
 
 
@@ -159,42 +181,61 @@ def show_details(play_song: str) -> None:
 
 def pause_music() -> None:
     global paused
-    paused = TRUE
+    paused = True
     mixer.music.pause()
     statusbar['text'] = "Music Paused"
 
 
 def play_music() -> None:
+    """if not playing: play, if playing and paused, unpause"""
     global paused
+    global playing
+    global current_song
+    try:
+        selected_song_from_box: Tuple[int] = playlistbox.curselection()
+        selected_song = int(selected_song_from_box[0])
+        play_it = playlist[selected_song]
+    except IndexError as e:  # no files in the playlistbox tuple
+        statusbar['text'] = "Choose a file from the playlist"
+        if DEBUG:
+            print(e)
+    except Exception as e:
+        messagebox.showerror('File not found, or unknown file type. Please check again.')
+        if DEBUG:
+            print(e)
 
-    if paused:
-        mixer.music.unpause()
-        statusbar['text'] = "Music Resumed"
-        paused = FALSE
-    else:
+    if not playing:  # if not yet playing, play selected song
         try:
             stop_music()
-            time.sleep(1)
-            selected_song_from_box: Tuple[int] = playlistbox.curselection()
-            selected_song = int(selected_song_from_box[0])
-            play_it = playlist[selected_song]
+            time.sleep(0.5)
             mixer.music.load(play_it)
             mixer.music.play()
-        except NameError as e:  # filename not defined
-            statusbar['text'] = "Please select a file to play"
-            print(e)
-        except IndexError as e:  # no files in the playlistbox tuple
-            statusbar['text'] = "Choose a file from the playlist"
-            print(e)
         except Exception as e:
             messagebox.showerror('File not found, or unknown file type. Please check again.')
-            print(e)
+            if DEBUG:
+                print(e)
         else:
+            playing = True
+            current_song = play_it
             show_details(play_it)
             statusbar['text'] = "Playing music" + ' - ' + os.path.basename(play_it)
+    elif playing and paused:  # if paused, resume
+        mixer.music.unpause()
+        statusbar['text'] = "Music Resumed"
+        paused = False
+    elif playing and not paused:
+        if current_song == play_it:  # if already playing song, do nothing
+            statusbar['text'] = "Playing music" + ' - ' + os.path.basename(play_it)
+        else:           # if different song selected, retry
+            playing = False
+            play_music()
 
 
 def stop_music() -> None:
+    global playing
+    global current_song
+    playing = False
+    current_song = None
     mixer.music.stop()
     statusbar['text'] = "Music Stopped"
 
@@ -205,7 +246,8 @@ def rewind_music() -> None:
 
 
 def set_vol(val: str) -> None:
-    volume = int(val) / 100
+    vol_float = float(val)
+    volume = int(vol_float) / 100
     mixer.music.set_volume(volume)
     # set_volume of mixer takes value only from 0 to 1
 
@@ -216,57 +258,62 @@ def mute_music() -> None:
         mixer.music.set_volume(0.7)
         volumeBtn.configure(image=volumePhoto)
         scale.set(70)
-        muted = FALSE
+        muted = False
     else:  # mute the music
         mixer.music.set_volume(0)
         volumeBtn.configure(image=mutePhoto)
         scale.set(0)
-        muted = TRUE
+        muted = True
 
 
-middleframe = tk.Frame(rightframe)
+middleframe = Frame(rightframe)
 middleframe.pack(pady=30, padx=30)
 
 
-playPhoto = PhotoImage(file='assets/icons/play.png')
+playPhoto = PhotoImage(file=mplayer_path + '/assets/icons/play.png')
 playBtn = Button(middleframe, image=playPhoto, command=play_music)
 playBtn.grid(row=0, column=1, padx=10)
 
-stopPhoto = PhotoImage(file='assets/icons/stop.png')
+stopPhoto = PhotoImage(file=mplayer_path + '/assets/icons/stop.png')
 stopBtn = Button(middleframe, image=stopPhoto, command=stop_music)
 stopBtn.grid(row=0, column=2, padx=10)
 
-pausePhoto = PhotoImage(file='assets/icons/pause.png')
+pausePhoto = PhotoImage(file=mplayer_path + '/assets/icons/pause.png')
 pauseBtn = Button(middleframe, image=pausePhoto, command=pause_music)
 pauseBtn.grid(row=0, column=3, padx=10)
 
 
-bottomframe = tk.Frame(rightframe)
+bottomframe = Frame(rightframe)
 bottomframe.pack()
 
 
-mutePhoto = PhotoImage(file='assets/icons/mute.png')
-volumePhoto = PhotoImage(file='assets/icons/volume.png')
+mutePhoto = PhotoImage(file=mplayer_path + '/assets/icons/mute.png')
+volumePhoto = PhotoImage(file=mplayer_path + '/assets/icons/volume.png')
 volumeBtn = Button(bottomframe, image=volumePhoto, command=mute_music)
 volumeBtn.grid(row=0, column=1)
 
 
-rewindPhoto = PhotoImage(file='assets/icons/rewind.png')
+rewindPhoto = PhotoImage(file=mplayer_path + '/assets/icons/rewind.png')
 rewindBtn = Button(bottomframe, image=rewindPhoto, command=rewind_music)
 rewindBtn.grid(row=0, column=0)
 
 scale = Scale(bottomframe, from_=0, to=100, orient=HORIZONTAL, command=set_vol)
 scale.set(init_vol)
-mixer.music.set_volume(init_vol/100)
+mixer.music.set_volume(init_vol / 100)
 scale.grid(row=0, column=2, pady=15, padx=30)
 
 
 def on_closing() -> None:
-    stop_music()
-    root.destroy()
-    print('App closed')
+    try:
+        stop_music()
+        root.destroy()
+    except Exception as e:
+        if DEBUG: print(e)
+    else:
+        print('App closed')
 
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 if __name__ == "__main__":
-    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
--- tkinter/mplayer/tests.py
+++ tkinter/mplayer/tests.py
@@ -1,1 +1,14 @@
-import pytest
+from pytest import approx
+
+from . import mplay
+
+
+class TestMplayer():
+
+    def test1(self) -> None:
+        assert (5 * 0.1) == approx(0.5)
+
+    def test_init_values(self) -> None:
+        assert mplay.init_vol == 70
+        assert mplay.paused is False
+        assert mplay.muted is False
