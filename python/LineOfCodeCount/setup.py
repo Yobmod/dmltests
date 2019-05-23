f612da3699fd81802530a291bcffa1277fdc501e
yobmod <yobmod@gmail.com>
2019-05-23 23:59:48 +0100

    .

--- python/LineOfCodeCount/setup.py
+++
@@ -1,8 +0,0 @@
-try:
-	from setuptools import setup		# type: ignore
-except ImportError:
-	from distutils.core import setup
-from Cython.Build import cythonize		# type: ignore
-
-setup(
-	ext_modules=cythonize('LOC_count.pyx'))
