4275e53cdfbdaab206d3ee684ef76edfeba0aaa2
yobmod <yobmod@gmail.com>
2019-10-27 01:17:38 +0100

    add pyside calculator

---
+++ pyside/calc/pycalc.py
@@ -0,0 +1,128 @@
+"""PyCalc is a simple calculator built using Python and PyQt5."""
+
+import sys
+from functools import partial
+
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QWidget, QGridLayout, QVBoxLayout, QPushButton, QLineEdit
+
+from typing import Dict, Tuple, cast
+
+
+class PyCalcUi(QMainWindow):
+    """PyCalc's View (GUI)."""
+
+    def __init__(self) -> None:
+        """View initializer."""
+        super().__init__()
+        # Set some main window's properties
+        self.setWindowTitle('PyCalc')
+        self.setFixedSize(235, 235)
+        # Set the central widget
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+        self.generalLayout = QVBoxLayout()
+        self._centralWidget.setLayout(self.generalLayout)
+        # Create the display and the buttons
+        self._createDisplay()
+        self._createButtons()
+
+    def _createDisplay(self) -> None:
+        """Create the display."""
+        # Create the display widget
+        self.display = QLineEdit()
+        # Set some display's properties
+        self.display.setFixedHeight(35)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        # Add the display to the general layout
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self) -> None:
+        """Create the buttons."""
+        self.buttons: Dict[str, QPushButton] = {}
+        buttonsLayout = QGridLayout()
+        # Button text | position on the QGridLayout
+        buttons: Dict[str, Tuple[int, int]] = {'7': (0, 0),
+                                               '8': (0, 1),
+                                               '9': (0, 2),
+                                               '/': (0, 3),
+                                               'C': (0, 4),
+                                               '4': (1, 0),
+                                               '5': (1, 1),
+                                               '6': (1, 2),
+                                               '*': (1, 3),
+                                               '(': (1, 4),
+                                               '1': (2, 0),
+                                               '2': (2, 1),
+                                               '3': (2, 2),
+                                               '-': (2, 3),
+                                               ')': (2, 4),
+                                               '0': (3, 0),
+                                               '00': (3, 1),
+                                               '.': (3, 2),
+                                               '+': (3, 3),
+                                               '=': (3, 4),
+                                               }
+        # Create the buttons and add them to the grid layout
+        for btnText, pos in buttons.items():
+            self.buttons[btnText] = QPushButton(btnText)
+            self.buttons[btnText].setFixedSize(40, 40)
+            buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+            # Add buttonsLayout to the general layout
+            self.generalLayout.addLayout(buttonsLayout)
+
+    def setDisplayText(self, text: str) -> None:
+        """Set display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def displayText(self) -> str:
+        """Get display's text."""
+        return cast(str, self.display.text())
+
+    def clearDisplay(self) -> None:
+        """Clear the display."""
+        self.setDisplayText('')
+
+
+class PyCalcCtrl:
+    """PyCalc Controller class."""
+
+    def __init__(self, view: PyCalcUi) -> None:
+        """Controller initializer."""
+        self._view = view
+        # Connect signals and slots
+        self._connectSignals()
+
+    def _buildExpression(self, sub_exp: str) -> None:
+        """Build expression."""
+        expression = self._view.displayText() + sub_exp
+        self._view.setDisplayText(expression)
+
+    def _connectSignals(self) -> None:
+        """Connect signals and slots."""
+        for btnText, btn in self._view.buttons.items():
+            if btnText not in {'=', 'C'}:
+                btn.clicked.connect(partial(self._buildExpression, btnText))
+
+        self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+
+
+def main() -> None:
+    """Main function."""
+    # Create an instance of QApplication
+    pycalc = QApplication(sys.argv)
+    # Show the calculator's GUI
+    view = PyCalcUi()
+    view.show()
+    # Create instances of the model and the controller
+    PyCalcCtrl(view=view)
+
+    # Execute the calculator's main loop
+    sys.exit(pycalc.exec_())
+
+
+if __name__ == '__main__':
+    main()
--- pyside/form/main.py
+++ pyside/form/main.py
@@ -1,11 +1,14 @@
 import sys
 import json
 
+import sqlite3
+from sqlite3 import Error
+
 from PySide2.QtWidgets import QDialog, QApplication
 from PySide2.QtWidgets import QHBoxLayout, QVBoxLayout
 from PySide2.QtWidgets import QLineEdit, QLabel, QPushButton
 
-from typing import Optional as Opt
+from typing import Optional as Opt, Dict, Union
 
 
 class Form(QDialog):
@@ -45,14 +48,55 @@ class Form(QDialog):
         self.setLayout(main_layout)
 
     def save_entries(self) -> None:
+        """save entry as json file and to SQLite database"""
         entry = {"name": self.name.text(),
                  "address": self.address.text(),
                  "phone": self.phone.text()}
         print(f"Hello {entry['name']}. Your address is {entry['address']} and phone number is {entry['phone']}")
 
-        with open(Rf".\data\entry_{entry['name']}.json", "w") as file:
-            entry_json = json.dumps(entry)
+        # TODO check data folder exists or create
+        # TODO pass data path as arg to json and db funcs
+
+        self.save_as_json(entry)
+        self.save_to_db(entry)
+
+
+    def save_as_json(self, entry_dict: Dict[str, Union[str, int]]) -> None:
+        with open(Rf".\data\entry_{entry_dict['name']}.json", "w") as file:
+            entry_json = json.dumps(entry_dict)
             file.write(entry_json)
+
+
+    def save_to_db(self, entry_dict: Dict[str, Union[str, int]]) -> None: 
+        db_conn = self.create_connection(R".\data\pythonsqlite.db")
+        if db_conn is not None:
+            self.create_table(db_conn)
+            db_conn.close
+
+
+    def create_connection(self, db_file: str) -> Opt[sqlite3.Connection]:
+        """ create a database connection to a SQLite database """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+            print(f"Database version SQLite {sqlite3.version}")
+        except Error as e:
+            print(e)
+            return None
+        else: 
+            return conn
+
+    def create_table(self, conn: sqlite3.Connection) -> None:
+        create_table_sql = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        address text,
+                                        phone text ); """
+        try:
+            c = conn.cursor()
+            c.execute(create_table_sql)
+        except Error as e:
+            print(e)
 
 
 if __name__ == "__main__":
