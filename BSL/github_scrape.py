b1097d2dd7e550cda25ffb8bc50968e5cd4bc6ab
yobmod <yobmod@gmail.com>
2019-11-08 22:22:34 +0000

    add BSL scraping project

--- BSL/github_scrape.py
+++ BSL/github_scrape.py
@@ -2,8 +2,18 @@
 
 from __future__ import print_function
 import argparse
-import mechanicalsoup
+import mechanicalsoup as ms
 from getpass import getpass
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bs4 import BeautifulSoup as bs
+    from requests import Response
+
+    class MSResponse(Response):
+        soup = ms.Brower.get_soup()
+
 
 parser = argparse.ArgumentParser(description="Login to GitHub.")
 parser.add_argument("username")
@@ -11,7 +21,7 @@ args = parser.parse_args()
 
 args.password = getpass("Please enter your GitHub password: ")
 
-browser = mechanicalsoup.StatefulBrowser(
+browser = ms.StatefulBrowser(
     soup_config={'features': 'lxml'},
     raise_on_404=True,
     user_agent='MyBot/0.1: mysite.example.com/bot_info',
@@ -24,13 +34,13 @@ browser.follow_link("login")
 browser.select_form('#login form')
 browser["login"] = args.username
 browser["password"] = args.password
-resp = browser.submit_selected()
+resp: 'MSResponse' = browser.submit_selected()
 
 # Uncomment to launch a web browser on the current page:
 # browser.launch_browser()
 
 # verify we are now logged in
-page = browser.get_current_page()
+page: 'bs' = browser.get_current_page()
 messages = page.find("div", class_="flash-messages")
 if messages:
     print(messages.text)
@@ -45,7 +55,8 @@ print(page.title.text)
 
 # verify we remain logged in (thanks to cookies) as we browse the rest of
 # the site
-page3 = browser.open("https://github.com/MechanicalSoup/MechanicalSoup")
-assert page3.soup.select(".logout-form")
+resp2: 'MSResponse' = browser.open("https://github.com/MechanicalSoup/MechanicalSoup")
+soup2: 'bs' = resp2.soup  
+assert soup2.select(".logout-form")
 
 print("done")
