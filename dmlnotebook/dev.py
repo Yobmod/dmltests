644f7f2dd806439d754a7ea267cf9efd7bdd7ac0
yobmod <yobmod@gmail.com>
2020-03-28 16:13:27 +0000

    DML, 2020-03-28 16:13:27.611082

--- dmlnotebook/dev.py
+++ dmlnotebook/dev.py
@@ -62,11 +62,8 @@ from pathlib import Path
 venv_path = subprocess.check_output("poetry env info --path".split(), shell=True)
 venv_path = venv_path.decode("UTF-8")
 
-settings = dict()
-
 Path(".vscode").mkdir(parents=True, exist_ok=True)
 Path(".vscode/settings.json").touch()
-
 
 with open(".vscode/settings.json", "r") as f:
     settings = json.load(f)
